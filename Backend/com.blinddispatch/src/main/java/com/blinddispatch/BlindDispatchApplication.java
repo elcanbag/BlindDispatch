b16ab931350335580b2b21c3fe892fe7ed01a780
elcanbag <elcanbagirov09@gmail.com>
2025-04-15 21:03:54 +0400

    first commit

---
+++ Backend/com.blinddispatch/src/main/java/com/blinddispatch/BlindDispatchApplication.java
@@ -0,0 +1,13 @@
+package com.blinddispatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindDispatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlindDispatchApplication.class, args);
+	}
+
+}
